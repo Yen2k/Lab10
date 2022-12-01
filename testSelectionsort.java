0be8564c900c45068bdabc5ee527a5d49333f619
Yen <yenduong2k@gmail.com>
2022-12-01 16:44:34 -0500

    ProgrammerA worked on testPositive() and testNegative(). Programmer A changed Selectionsort.java

--- testSelectionsort.java
+++ testSelectionsort.java
@@ -14,28 +14,53 @@ public class testSelectionSort {
     public testSelectionSort() {
     }
 
-    public void testPositive(){
-        int[] arr = new int[5];
-        arr[0] = 8;
-        arr[1] = 9;
-        arr[2] = 7;
-        arr[3] = 10;
-        arr[4] = 2;
+    public void testPositive() {
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
 
-        int[] Sortedarr = new int[5];
-        Sortedarr[0] = 2;
-        Sortedarr[1] = 7;
-        Sortedarr[2] = 8;
-        Sortedarr[3] = 9;
-        Sortedarr[4] = 10;
+		int[] ExpectedSortedArr = new int[5];
+		ExpectedSortedArr[0] = 2;
+		ExpectedSortedArr[1] = 7;
+		ExpectedSortedArr[2] = 8;
+		ExpectedSortedArr[3] = 9;
+		ExpectedSortedArr[4] = 10;
 
-        /** add tests to check for this unit test **/
-    }
+		/** add tests to check for this unit test **/
+		SelectionSort sortArr = new SelectionSort();
+		int[] ActualSortArr = sortArr.basicSelectionSort(arr);
+		/** add tests to check for this unit test **/
+//		assert(Arrays.equals(sosrtedValue, Sortedarr));
+		assertArrayEquals(ExpectedSortedArr, ActualSortArr);
+	}
 
-    public void testNegative(){
+	public void testNegative() {
 
-        /** Test data contains negative values only **/
-    }
+		/** Test data contains negative values only **/
+		int[] arr = new int[5];
+		arr[0] = 8;
+		arr[1] = 9;
+		arr[2] = 7;
+		arr[3] = 10;
+		arr[4] = 2;
+
+		int[] ExpectedSortedArr = new int[5];
+		ExpectedSortedArr[0] = 2;
+		ExpectedSortedArr[1] = 7;
+		ExpectedSortedArr[2] = 8;
+		ExpectedSortedArr[3] = 9;
+		ExpectedSortedArr[4] = 10;
+
+		/** add tests to check for this unit test **/
+		SelectionSort sortArr = new SelectionSort();
+		int[] ActualSortArr = sortArr.basicSelectionSort(arr);
+		/** add tests to check for this unit test **/
+//		assert(Arrays.equals(sosrtedValue, Sortedarr));
+		assertArrayEquals(ExpectedSortedArr, ActualSortArr);
+	}
 
     public void testMixed(){
 
